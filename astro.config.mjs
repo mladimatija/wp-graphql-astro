25070e064ab53255eea8cc64f8e4908945809642
Matija Culjak <matija.culjak@gmail.com>
2023-01-17 00:34:46 +0100

    remove astro netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,8 @@ import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
 // https://astro.build/config
-
-// https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://matijaculjak.com",
   output: "static",
   integrations: [react(), sitemap()],
-  adapter: netlify()
 });
